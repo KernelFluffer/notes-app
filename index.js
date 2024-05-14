b4d1e16807c596859dcee340f320c9b1952858ba
kernelfluffer <kernelfluffer@gmail.com>
2024-05-14 23:07:14 +0200

    almost done, Get rid of the errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const noteInput = document.getElementById('note-input');
 const noteForm = document.getElementById('note-form');
-const noteContainer = document.getElementById('note-container')
+const noteContainer = document.getElementById('note-container');
 
 function displayNotes() {
     const notes = JSON.parse(localStorage.getItem('notes')) || [];
@@ -8,15 +8,39 @@ function displayNotes() {
     noteContainer.innerHTML = '';
 
     notes.forEach(note => {
-        const noteElement = document
+        const noteElement = document.createElement('button');
         noteElement.classList.add('note');
         noteElement.textContent = note;
 
-        const deleteButton = document.createElement('button')
+        const deleteButton = document.createElement('button');
         deleteButton.textContent = 'X';
         deleteButton.addEventListener('click', () => {
             const newNotes = notes.filter(n => n !== note);
-            localStorage.setItem('notes', JSON.stringify)
+            localStorage.setItem('notes', JSON.stringify(notes));
+            displayNotes();
         });
+        noteElement.appendChild(deleteButton);
+
+        noteContainer.appendChild(noteContainer);
     });
 }
+
+displayNotes();
+
+noteForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const note = noteInput.value.trim();
+
+    if (note) {
+        const notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+        notes.push(note);
+
+        localStorage.setItem('notes', JSON.stringify(notes));
+
+        noteInput.value = '';
+        displayNotes();
+    }
+    
+})
